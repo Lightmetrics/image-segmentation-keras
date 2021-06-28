f86404ff80a067071556786688166c374c8df5fd
Anant Mittal <ai.with.anant@gmail.com>
2021-06-28 09:58:59 +0530

    Add merge hough lines helper functions

--- keras_segmentation/predict.py
+++ keras_segmentation/predict.py
@@ -319,3 +319,89 @@ def evaluate(model=None, inp_images=None, annotations=None,
         "mean_IU": mean_IU,
         "class_wise_IU": cl_wise_score
     }
+
+def _check_overlap(line1, line2):
+    combination = np.array([line1,
+                            line2,
+                            [line1[0], line1[1], line2[0], line2[1]],
+                            [line1[0], line1[1], line2[2], line2[3]],
+                            [line1[2], line1[3], line2[0], line2[1]],
+                            [line1[2], line1[3], line2[2], line2[3]]])
+    distance = np.sqrt((combination[:,0] - combination[:,2])**2 
+    + (combination[:,1] - combination[:,3])**2)
+    max = np.amax(distance)
+    overlap = distance[0] + distance[1] - max
+    endpoint = combination[np.argmax(distance)]
+    return (overlap >= 0), endpoint #replace 0 with the value of distance between 2 collinear lines
+
+def _merge_lines(line_list):
+    #convert (x1, y1, x2, y2) form to (r, alpha) form
+    if line_list is not None:
+        line_list = np.squeeze(line_list, axis=1)
+        line_list = np.array(line_list)
+        A = line_list[:,1] - line_list[:,3]
+        B = line_list[:,2] - line_list[:,0]
+        C = line_list[:,0]*line_list[:,3] - line_list[:,2]*line_list[:,1]
+        r = np.divide(np.abs(C), np.sqrt(A*A+B*B))
+        alpha = (np.arctan2(-B,-A) + math.pi) % (2*math.pi) - math.pi
+        r_alpha = np.column_stack((r, alpha))
+
+        #prepare some variables to keep track of lines looping
+        r_bin_size = 10 #maximum distance to treat 2 lines as one
+        alpha_bin_size = 0.15 #maximum angle (radian) to treat 2 lines as one
+        merged = np.zeros(len(r_alpha), dtype=np.uint8)
+        line_group = np.empty((0,4), dtype=np.int32)
+        group_count = 0
+
+        for line_index in range(len(r_alpha)): 
+            if merged[line_index] == 0: #if line hasn't been merged yet
+                merged[line_index] = 1
+                line_group = np.append(line_group, [line_list[line_index]], axis=0)
+                for line_index2 in range(line_index+1,len(r_alpha)):
+                    if merged[line_index2] == 0:
+                        #calculate the differences between 2 lines by r and alpha
+                        dr = abs(r_alpha[line_index,0] - r_alpha[line_index2,0])
+                        dalpha = abs(r_alpha[line_index,1] - r_alpha[line_index2,1])
+                        if (dr<r_bin_size) and (dalpha<alpha_bin_size): # if close, check overlap
+                            overlap = check_overlap(line_group[group_count] 
+			    endpoints = line_list[line_index2])
+                            if overlap:
+                                line_group[group_count] = endpoints
+                                merged[line_index2] = 1
+                group_count += 1
+        line_group = np.expand_dims(line_group, axis=1)
+        return line_group
+    else:
+        return None 
+
+
+def evaluate_egolanes(model=None, inp_images=None, annotations=None,
+             inp_images_dir=None, annotations_dir=None, checkpoints_path=None, read_image_type=1):
+
+    if model is None:
+        assert (checkpoints_path is not None),\
+                "Please provide the model or the checkpoints_path"
+        model = model_from_checkpoint_path(checkpoints_path)
+
+    if inp_images is None:
+        assert (inp_images_dir is not None),\
+                "Please provide inp_images or inp_images_dir"
+        assert (annotations_dir is not None),\
+            "Please provide inp_images or inp_images_dir"
+
+        paths = get_pairs_from_paths(inp_images_dir, annotations_dir)
+        paths = list(zip(*paths))
+        inp_images = list(paths[0])
+        annotations = list(paths[1])
+
+    assert type(inp_images) is list
+    assert type(annotations) is list
+
+    for inp, ann in tqdm(zip(inp_images, annotations)):
+        pr = predict(model, inp, read_image_type=read_image_type)
+        gt = get_segmentation_array(ann, model.n_classes,
+                                    model.output_width, model.output_height,
+                                    no_reshape=True, read_image_type=read_image_type)
+        gt = gt.argmax(-1)
+        # call a function to get x1, x2 and vx and vy for both and gt and pr, and find the error
+    return None
