bdc4708b3478b5bae54a769ba9e98bdb2972e6c5
Anant Mittal <ai.with.anant@gmail.com>
2021-06-29 10:18:40 +0530

    Add: egolanes estimate and evaluate function

--- keras_segmentation/predict.py
+++ keras_segmentation/predict.py
@@ -9,6 +9,11 @@ import numpy as np
 from tqdm import tqdm
 from time import time
 
+import statistics
+import math
+import warnings
+from scipy.signal import find_peaks
+
 from .train import find_latest_checkpoint
 from .data_utils.data_loader import get_image_array, get_segmentation_array,\
     DATA_LOADER_SEED, class_colors, get_pairs_from_paths
@@ -363,8 +368,7 @@ def _merge_lines(line_list):
                         dr = abs(r_alpha[line_index,0] - r_alpha[line_index2,0])
                         dalpha = abs(r_alpha[line_index,1] - r_alpha[line_index2,1])
                         if (dr<r_bin_size) and (dalpha<alpha_bin_size): # if close, check overlap
-                            overlap = check_overlap(line_group[group_count] 
-			    endpoints = line_list[line_index2])
+                            overlap, endpoints = _check_overlap(line_group[group_count], line_list[line_index2])
                             if overlap:
                                 line_group[group_count] = endpoints
                                 merged[line_index2] = 1
@@ -375,8 +379,109 @@ def _merge_lines(line_list):
         return None 
 
 
+
+def _get_egolanes_points(mask):
+    h, w, _ = mask.shape
+    image_l = (mask % 2)*100
+    image_r = (mask // 2)*100
+    uint8_img_l = np.uint8(image_l[:,:,0])
+    uint8_img_r = np.uint8(image_r[:,:,0])
+
+    lines_l = _merge_lines(cv2.HoughLinesP(uint8_img_l, 1, np.pi/30, 50, minLineLength=20, maxLineGap=10))
+    lines_r = _merge_lines(cv2.HoughLinesP(uint8_img_r, 1, np.pi/30, 50, minLineLength=20, maxLineGap=10))
+                                          
+    intersection_pts = 0
+    list_vx = []
+    list_vy = []
+    list_x1 = []
+    list_x2 = []
+    list_lw = [] # lane width
+    mode_vx, mode_vy = None,None
+    x1_intercept_mode, x2_intercept_mode = None, None
+
+    # if both the lines are predicted
+    if lines_l is not None and lines_r is not None:
+        for line_l in lines_l:
+            for line_r in lines_r:
+                x1_l, y1_l, x2_l, y2_l = line_l[0]
+                x1_r, y1_r, x2_r, y2_r = line_r[0]
+                l_slope, l_intercept = np.polyfit((x1_l, x2_l), (y1_l, y2_l), 1)
+                r_slope, r_intercept = np.polyfit((x1_r, x2_r), (y1_r, y2_r), 1)
+        vx = int((r_intercept - l_intercept) / (l_slope - r_slope))
+        vy = int(l_slope*((r_intercept - l_intercept)/ (l_slope - r_slope)) + l_intercept)
+        x1_intercept = int((h-1 - l_intercept)/l_slope)
+        x2_intercept = int((h-1 - r_intercept)/r_slope)
+        # append valid intercepts to the list
+        if x1_intercept > -3*w and x2_intercept > -3*w and x1_intercept < 3*w and x2_intercept < 3*w:
+            list_vx.append(vx)
+            list_vy.append(vy)
+            list_x1.append(x1_intercept)
+            list_x2.append(x2_intercept)
+            list_lw.append(abs(x2_intercept - x1_intercept))
+            intersection_pts += 1
+        # calculate vx based on mode
+        f_vx, b_vx = np.histogram(list_vx)
+        mode_vx = int(b_vx[np.argmax(f_vx)])
+        # calculate vy based on mode
+        f_vy, b_vy = np.histogram(list_vy)
+        mode_vy = int(b_vy[np.argmax(f_vy)])
+        # find x1 intercept based on mode
+        f_x1, b_x1 = np.histogram(list_x1, bins=np.arange(-3*w,3*w,w/20))
+        x1_intercept_mode = int(b_x1[np.argmax(f_x1)])
+        x1_intercept_multiple_modes = [b_x1[idx] for idx in find_peaks(f_x1, height=20)[0]]
+        # find x2 intercept based on mode
+        f_x2, b_x2 = np.histogram(list_x2, bins=np.arange(-3*w,3*w,w/20))
+        x2_intercept_mode = int(b_x2[np.argmax(f_x2)])
+        x2_intercept_multiple_modes = [b_x2[idx] for idx in find_peaks(f_x2, height=20)[0]]
+    else:
+        try:
+            if lines_l is not None:
+                for line_l in lines_l:
+                    x1_l, y1_l, x2_l, y2_l = line_l[0]
+                    l_slope, l_intercept = np.polyfit((x1_l, x2_l), (y1_l, y2_l), 1)
+                    x1_intercept = int((h-1 - l_intercept)/l_slope)
+                    vx =  int((0 - l_intercept)/l_slope)
+                    if x1_intercept > -3*w and x1_intercept < 3*w:
+                        list_x1.append(x1_intercept)
+                        list_vx.append(vx)
+            f_x1, b_x1 = np.histogram(list_x1, bins=np.arange(-3*w,3*w,w/20))
+            x1_intercept_mode = int(b_x1[np.argmax(f_x1)])
+            # calculate vx based on mode
+            f_vx, b_vx = np.histogram(list_vx)
+            mode_vx = int(b_vx[np.argmax(f_vx)])
+            x1_intercept_multiple_modes = [b_x1[idx] for idx in find_peaks(f_x1, height=20)[0]]
+        except Exception as e:
+            print("Error finding x1 intercepts when x2 is None: ", e)
+        try:
+            if lines_r is not None:
+                for line_r in lines_r:
+                    x1_r, y1_r, x2_r, y2_r = line_r[0]
+                    r_slope, r_intercept = np.polyfit((x1_r, x2_r), (y1_r, y2_r), 1)
+                    x2_intercept = int((h-1 - r_intercept)/r_slope)
+                    vx =  int((0 - r_intercept)/r_slope) 
+                if x2_intercept > -3*w and x2_intercept < 3*w:
+                        list_x2.append(x2_intercept)
+            f_x2, b_x2 = np.histogram(list_x2, bins=np.arange(-3*w,3*w,w/20))
+            x2_intercept_mode = int(b_x2[np.argmax(f_x2)])
+            # calculate vx based on mode
+            f_vx, b_vx = np.histogram(list_vx)
+            mode_vx = int(b_vx[np.argmax(f_vx)])
+            x1_intercept_multiple_modes = [b_x1[idx] for idx in find_peaks(f_x1, height=20)[0]]
+            x2_intercept_multiple_modes = [b_x2[idx] for idx in find_peaks(f_x2, height=20)[0]]
+        except Exception as e:
+            print("Error finding x2 intercepts when x1 is None: ", e)
+    
+    results = {"x1_intercept_mode": x1_intercept_mode, 
+    "x2_intercept_mode": x2_intercept_mode,
+    "x1_intercept_multiple_modes": x1_intercept_multiple_modes,
+    "x2_intercept_multiple_modes": x2_intercept_multiple_modes,
+    "vx_mode": mode_vx,
+    "vy_mode": mode_vy}
+    return results
+
+
 def evaluate_egolanes(model=None, inp_images=None, annotations=None,
-             inp_images_dir=None, annotations_dir=None, checkpoints_path=None, read_image_type=1):
+                      inp_images_dir=None, annotations_dir=None, out_dir=None, checkpoints_path=None, read_image_type=1):
 
     if model is None:
         assert (checkpoints_path is not None),\
@@ -397,11 +502,63 @@ def evaluate_egolanes(model=None, inp_images=None, annotations=None,
     assert type(inp_images) is list
     assert type(annotations) is list
 
+    errors = []
     for inp, ann in tqdm(zip(inp_images, annotations)):
         pr = predict(model, inp, read_image_type=read_image_type)
-        gt = get_segmentation_array(ann, model.n_classes,
-                                    model.output_width, model.output_height,
-                                    no_reshape=True, read_image_type=read_image_type)
+        
+        gt = get_segmentation_array(ann, model.n_classes, model.output_width, model.output_height, no_reshape=True, read_image_type=read_image_type) #one hot encoded array
         gt = gt.argmax(-1)
+        
+        inp_img = cv2.imread(inp)
+        inp_h, inp_w, _ = inp_img.shape
+
+        seg_out_pr = visualize_segmentation(pr, inp_img, n_classes=model.n_classes, colors=[(0, 0, 0), (1, 1, 1), (2, 2, 2)])
+        seg_out_gt = visualize_segmentation(gt, inp_img, n_classes=model.n_classes, colors=[(0, 0, 0), (1, 1, 1), (2, 2, 2)])
         # call a function to get x1, x2 and vx and vy for both and gt and pr, and find the error
+        egolanes_pts_gt = _get_egolanes_points(seg_out_gt)
+        egolanes_pts_pr = _get_egolanes_points(seg_out_pr)
+
+        error = 0
+        if egolanes_pts_gt["x1_intercept_mode"] is not None and egolanes_pts_pr["x1_intercept_mode"] is not None:
+            error += (egolanes_pts_gt["x1_intercept_mode"] - egolanes_pts_pr["x1_intercept_mode"])**2
+        if egolanes_pts_gt["x2_intercept_mode"] is not None and egolanes_pts_pr["x2_intercept_mode"] is not None:
+            error += (egolanes_pts_gt["x2_intercept_mode"] - egolanes_pts_pr["x2_intercept_mode"])**2
+        if egolanes_pts_gt["vx_mode"] is not None and egolanes_pts_pr["vx_mode"] is not None:
+            error += (egolanes_pts_gt["vx_mode"] - egolanes_pts_pr["vx_mode"])**2
+        if egolanes_pts_gt["vy_mode"] is not None and egolanes_pts_pr["vy_mode"] is not None:
+            error += (egolanes_pts_gt["vy_mode"] - egolanes_pts_pr["vy_mode"])**2
+        error = math.sqrt(error)
+        errors.append(error)
+        print("\nEgolanes GT", egolanes_pts_gt)
+        print("Egolanes PR", egolanes_pts_pr)
+        print("-"*50)
+        
+        if out_dir is not None:
+            os.makedirs(out_dir, exist_ok=True)
+            out_path = os.path.join(out_dir, f"e-{int(error):05}_{os.path.basename(inp)}")
+            
+	    #segmentation output overlayed on input image
+            fused_img = visualize_segmentation(pr, inp_img, n_classes=model.n_classes, overlay_img=True, 
+                                               colors=[(0, 0, 0), (0, 0, 255), (0, 0, 125)],
+                                               show_legends=False, class_names=["B", "L", "R"])
+            # code to annotate image with the ground truth
+            inp_img = cv2.circle(inp_img, (egolanes_pts_gt["vx_mode"], egolanes_pts_gt["vy_mode"]), 2, (0, 255, 0), 2)
+            cv2.line(inp_img, (egolanes_pts_gt["vx_mode"], 0), (egolanes_pts_gt["vx_mode"], inp_h-1), (0, 255, 0)) 
+            cv2.line(inp_img, (0, egolanes_pts_gt["vy_mode"]), (inp_w-1, egolanes_pts_gt["vy_mode"]), (0, 255, 0))
+            cv2.line(inp_img, (egolanes_pts_gt["x1_intercept_mode"], inp_h-1), (egolanes_pts_gt["vx_mode"], egolanes_pts_gt["vy_mode"]), (0, 255, 0), 2)
+            cv2.line(inp_img, (egolanes_pts_gt["x2_intercept_mode"], inp_h-1), (egolanes_pts_gt["vx_mode"], egolanes_pts_gt["vy_mode"]), (0, 255, 0), 2)
+            # code to annotate image with the predictions
+            if egolanes_pts_pr["vx_mode"] is not None and egolanes_pts_pr["vy_mode"] is not None:
+                inp_img = cv2.circle(inp_img, (egolanes_pts_pr["vx_mode"], egolanes_pts_pr["vy_mode"]), 2, (0, 0, 255), 2)
+                cv2.line(inp_img, (egolanes_pts_pr["vx_mode"], 0), (egolanes_pts_pr["vx_mode"], inp_h-1), (0, 0, 255)) 
+                cv2.line(inp_img, (0, egolanes_pts_pr["vy_mode"]), (inp_w-1, egolanes_pts_pr["vy_mode"]), (0, 0, 255))
+                cv2.line(inp_img, (egolanes_pts_pr["x1_intercept_mode"], inp_h-1), (egolanes_pts_pr["vx_mode"], egolanes_pts_pr["vy_mode"]), (0, 0, 255), 2)
+                cv2.line(inp_img, (egolanes_pts_pr["x2_intercept_mode"], inp_h-1), (egolanes_pts_pr["vx_mode"], egolanes_pts_pr["vy_mode"]), (0, 0, 255), 2)
+            elif egolanes_pts_pr["x1_intercept_mode"] is not None:
+                cv2.line(inp_img, (egolanes_pts_pr["x1_intercept_mode"], inp_h-1), (egolanes_pts_pr["vx_mode"], 0), (0, 0, 255), 2)
+            elif egolanes_pts_pr["x2_intercept_mode"] is not None: 
+                cv2.line(inp_img, (egolanes_pts_pr["x2_intercept_mode"], inp_h-1), (egolanes_pts_pr["vx_mode"], 0), (0, 0, 255), 2)
+            out_img = np.concatenate([fused_img, inp_img], axis=1)
+            cv2.imwrite(out_path, out_img)
+ 
     return None
